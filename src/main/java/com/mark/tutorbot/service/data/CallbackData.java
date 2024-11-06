347d91b2c258aee708ae3bb67ae404b9deb57b3a
Mark Krasnokuysky <Mark120320041234@mail.ru>
2024-11-07 03:06:41 +0500

    #11 Тестирование функционала путём добавление клавиатуры с callbackData в метод start перехватчика команд
    #11 Внедрение менеджеров в перехватчик callbackQuery-запросов.
    
    #11 Описание менеджеров для реализации логики сценариев нажатия кнопок клавиатуры путём редактированных сообщений.
    
    #11 Внедрение класса для хранения типов callbackData(запросов).

---
+++ src/main/java/com/mark/tutorbot/service/data/CallbackData.java
@@ -0,0 +1,6 @@
+package com.mark.tutorbot.service.data;
+
+public class CallbackData {
+    public static final String FEEDBACK = "feedback";
+    public static final String HELP = "help";
+}
---
+++ src/main/java/com/mark/tutorbot/service/manager/FeedbackManager.java
@@ -0,0 +1,38 @@
+package com.mark.tutorbot.service.manager;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
+
+@Component
+public class FeedbackManager {
+
+    public BotApiMethod<?> answerCommand(Message message) {
+        return SendMessage.builder()
+                .chatId(message.getChatId())
+                .text("""
+                        📍 Ссылки для обратной связи
+                        GitHub - https://github.com/markkrasnokutsky
+                        Telegram - https://t.me/emberquency
+                        """)
+                .disableWebPagePreview(true)
+                .build();
+    }
+
+    public BotApiMethod<?> answerCallbackQuery(CallbackQuery callbackQuery) {
+        return EditMessageText.builder()
+                .chatId(callbackQuery.getMessage().getChatId())
+                .messageId(callbackQuery.getMessage().getMessageId())
+                .text("""
+                        📍 Ссылки для обратной связи
+                        GitHub - https://github.com/markkrasnokutsky
+                        Telegram - https://t.me/emberquency
+                        """)
+                .disableWebPagePreview(true)
+                .build();
+    }
+
+}
