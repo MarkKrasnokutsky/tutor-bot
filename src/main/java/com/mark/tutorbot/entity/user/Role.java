36d561e76f208e03bd0d1dabc70b9b988b6e5afd
Mark Krasnokuysky <Mark120320041234@mail.ru>
2024-11-11 22:24:10 +0500

    #19 Включение аспектов на высоком уровне конфигурации приложения.
    #19 Создание аспекта для выполнения метода диспетчера действий телеграм-бота
    
    #19 Инициализация UserDetails и его репозитория. Перенос сущности User в отдельную директорию.
    
    #19 Добавление перечислений ролей и действий для пользователя.

---
+++ src/main/java/com/mark/tutorbot/entity/user/Role.java
@@ -0,0 +1,5 @@
+package com.mark.tutorbot.entity.user;
+
+public enum Role {
+    USER, ADMIN;
+}
--- src/main/java/com/mark/tutorbot/repository/DetailsRepository.java
+++ src/main/java/com/mark/tutorbot/repository/DetailsRepository.java
@@ -1,9 +1,14 @@
 package com.mark.tutorbot.repository;
 
-import com.mark.tutorbot.entity.user.User;
+import com.mark.tutorbot.entity.user.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface DetailsRepository extends JpaRepository<UserDetails, UUID> {
+
+
+
 }
