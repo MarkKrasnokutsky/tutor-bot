f5b990bb5f4bd548b7ab13d737d05f8d6984d4d0
Mark Krasnokuysky <Mark120320041234@mail.ru>
2024-11-11 21:38:58 +0500

    #17 Тестирование работы БД.
    #17 Создание репозитория для сущности пользователя.
    
    #17 Создание сущности пользователя.
    
    #17 Добавление зависимостей.
    
    #17 Конфигурация базы данных.

---
+++ src/main/java/com/mark/tutorbot/entity/User.java
@@ -0,0 +1,23 @@
+package com.mark.tutorbot.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @Column(name = "id")
+    private Long chatId;
+
+}
